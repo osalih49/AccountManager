c10a37b2b503e5b3661da91c9ba82d9d261742dc
Omar Salih <oosalih98@gmail.com>
2026-02-14 17:13:46 -0500

    Cleaned project, added account features

--- AccountService.cs
+++ AccountService.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace AccountManager
 {
@@ -11,6 +12,7 @@ namespace AccountManager
 
         public static void CreateAccount(Dictionary<string, UserAccount> accounts, string filePath)
         {
+            Console.Clear();
             Console.WriteLine("");
             Console.WriteLine("Welcome! Please Create your account below!");
             Console.WriteLine("");
@@ -23,6 +25,7 @@ namespace AccountManager
             if(string.IsNullOrWhiteSpace(userName))
             {
                 Console.WriteLine("Username cannot be empty, try again");
+                Thread.Sleep(2000);
                 return;
             }
 
@@ -30,6 +33,7 @@ namespace AccountManager
             if (accounts.ContainsKey(userName))
             {
                 Console.WriteLine("This username already exists");
+                Thread.Sleep(2000);
                 return;
             }
 
@@ -42,6 +46,7 @@ namespace AccountManager
             if(string.IsNullOrWhiteSpace(password))
             {
                 Console.WriteLine("Password cannot be empty or white space try again!");
+                Thread.Sleep(2000);
                 return;
             }
 
@@ -59,34 +64,101 @@ namespace AccountManager
                 else
                 {
                     Console.WriteLine("Passwords do not match. Please Try again");
+                    Thread.Sleep(2000);
                     confirmAttempts++;
                 }
             
             }
 
-            accounts[userName] = new UserAccount { Username = userName };
-            accounts[userName] = new UserAccount { Password =  password };
-            accounts[userName] = new UserAccount { Balance = 0 };
-            accounts[userName] = new UserAccount { IsActive = true };
-
+            accounts[userName] = new UserAccount { Username = userName, Password = password, Balance = 0, IsActive = true };
             StorageService.SaveUserAccount(accounts, filePath);
             Console.WriteLine("Account successfully created!");
-            Console.Write("");
+            Thread.Sleep(2000);
+            Console.Clear();
         }
-        public static void ViewBalance(Dictionary<string, UserAccount> accounts, string currentUser)
-        {
-            currentUser = accounts[currentUser].Username;
-            var currentBalance = accounts[currentUser].Balance;
 
+        public static UserAccount Login(Dictionary<string, UserAccount> accounts, string filePath)
+        {
+            Console.Clear();
+            Console.WriteLine(" == Welcome, Please Log-In! == ");
+            Console.Write("Username: ");
+            string userName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                Console.WriteLine("Username cannot be empty!");
+                Thread.Sleep(2000);
+                Console.Clear();
+                return null;
+            }
+            if (!accounts.ContainsKey(userName))
+            {
+                Console.WriteLine("This username does not exist.");
+                Thread.Sleep(2000);
+                Console.Clear();
+                return null;
+
+            }
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                Console.WriteLine("Password cannot be empty");
+                Thread.Sleep(2000);
+                Console.Clear();
+                return null; 
+            }
+
+            if (accounts[userName].Password != password)
+            {
+                Console.WriteLine("Invalid Password");
+                Thread.Sleep(2000);
+                Console.Clear();
+                return null;
+            }
+            Console.WriteLine("Logged in Successfully");
+            Thread.Sleep(2000);
+            Console.Clear();
+            return accounts[userName];
+             
+        }
+        public static void ViewBalance(Dictionary<string, UserAccount> accounts, string filePath, string currentUser)
+        {
+            Console.Clear();
+            var currentBalance = accounts[currentUser].Balance;
             Console.WriteLine($"Welcome {currentUser}");
             Console.WriteLine($"Your Current Balance = ${currentBalance}");
-
-
+            StorageService.SaveUserAccount(accounts, filePath);
 
         }
 
-        public static void Deposit(Dictionary<string, UserAccount> accounts, string filePath) 
-        { 
+        public static void Deposit(Dictionary<string, UserAccount> accounts, string currentUser) 
+        {
+            Console.Clear();
+            Console.WriteLine("== How much would you like to deposit? ==");
+            Console.Write("Depoit: $");
+            string deposit = Console.ReadLine();
+            if (int.TryParse(deposit, out int depositAmount))
+            {
+                if (depositAmount < 9)
+                {
+                    Console.WriteLine("WARNING! CANNOT BE A NEGATIVE NUMBER");
+                }
+                if(depositAmount > 0)
+                {
+                    accounts[currentUser].Balance = accounts[currentUser].Balance + depositAmount;
+                    Console.WriteLine($"You deposited {depositAmount}");
+                    Console.WriteLine($"Your current balance: {accounts[currentUser].Balance}");
+                 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid number");
+            }
+
             
         }
 
--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@ namespace AccountManager
     {
         static void Main(string[] args)
         {
+            //test
             string filePath = "accounts.json";
             Dictionary<string, UserAccount> accounts = StorageService.Accounts(filePath);
 
             string currentUser = null;
+            Console.Clear();
 
             while (true)
             {
@@ -30,10 +32,19 @@ namespace AccountManager
                 if (choice == "1")
                 {
                     AccountService.CreateAccount(accounts, filePath);
+
                 }
                 else if (choice == "2")
                 {
-                    LoggedInMenu(accounts, filePath ,currentUser);
+                    var currentUserAccount = AccountService.Login(accounts, filePath);
+                    if(currentUserAccount != null)
+                    {
+                        LoggedInMenu(accounts, filePath, currentUserAccount.Username);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Log-in Failed");
+                    }
                 }
                 else if (choice == "3")
                 {
@@ -45,45 +56,78 @@ namespace AccountManager
 
            
         }
-        static string LoggedInMenu(Dictionary<string, UserAccount> accounts, string filePath, string currentUser)
+        static string  LoggedInMenu(Dictionary<string, UserAccount> accounts, string filePath, string currentUser)
         {
+            Console.Clear();
+            while (true)
+            {
+                
+                Console.WriteLine($"Welcome {currentUser} to your Account!");
+                Console.WriteLine("");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("What would you like to do today?");
+                Console.WriteLine("");
+                Console.WriteLine("1) View Balance");
+                Console.WriteLine("2) Deposit");
+                Console.WriteLine("3) Close your Account");
+                Console.WriteLine("4) Logout");
+                Console.WriteLine("5) Exit");
+                Console.WriteLine("");
+                Console.WriteLine("----------------------------------------");
 
-            Console.WriteLine($"Welcome {currentUser} to your Account!");
-            Console.WriteLine("");
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("What would you like to do today?");
-            Console.WriteLine("");
-            Console.WriteLine("1) View Balance");
-            Console.WriteLine("2) Deposit");
-            Console.WriteLine("3) Close your Account");
-            Console.WriteLine("4) Logout");
-            Console.WriteLine("5) Exit");
-            Console.WriteLine("");
-            Console.WriteLine("----------------------------------------");
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.Clear();
+                    AccountService.ViewBalance(accounts, filePath, currentUser);
+                    Console.WriteLine("");
+                    Console.WriteLine("Press 'B' to go back");
+                    string backChoice = Console.ReadLine()?.Trim().ToUpper(); ;
 
-            string choice = Console.ReadLine();
-            if (choice == "1")
-            {
-                AccountService.ViewBalance(accounts, currentUser);
-            }
-            else if (choice == "2")
-            {
-                AccountService.Deposit(accounts, currentUser);
-            }
-            else if (choice == "3")
-            {
-                AccountService.CloseAccount(accounts, currentUser);
-            }
-            else if (choice == "4")
-            {
-                Console.WriteLine($"Thank you for visiting {currentUser} have an amazing day!");
+                    if (backChoice == "B")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                   
+                }
+                else if (choice == "2")
+                {
+                    Console.Clear();
+                    AccountService.Deposit(accounts, currentUser);
+                    Console.WriteLine("");
+                    Console.WriteLine("Press 'B' to go back");
+                    string backChoice = Console.ReadLine()?.Trim()?.ToUpper();
+
+                    if (backChoice == "B")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                }
+                else if (choice == "3")
+                {
+                    Console.Clear();
+                    AccountService.CloseAccount(accounts, currentUser);
+                }
+                else if (choice == "4")
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Thank you for visiting {currentUser} have an amazing day!");
+                    break;
+                }
+                else if (choice == "5")
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                }
             }
 
             return currentUser;
         }
 
-
+        }
     }
-}
+
 
 
